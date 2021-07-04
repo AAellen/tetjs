49e35dbbf59b81252abee480c6b367968d49489e
AAellen <aaellen01@gmail.com>
2021-07-04 21:30:30 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+const PORT = 3638;
+const domain="localhost";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) =>{
+    res.render("landing");
+});
+
+app.get("/play", (req, res) =>{
+    res.render("game");
+});
+
+app.listen( PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+});
